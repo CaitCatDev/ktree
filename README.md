230f9ad3435f77b8eef4df3e35a64e94bf313685
cat <caitcatdev@protonmail.com>
2023-05-24 05:06:07 +0100

    Add a bundle of features

--- README.md
+++ README.md
@@ -1,21 +1,64 @@
 # ktree:
 A simple lua file manager for NVIM
 
-## How to install 
+## How to install
 
-add a entry for your plugin manager or manually install it. 
+add a entry for your plugin manager or manually install it.
 For example you could place it `~/.config/nvim/lua/ktree`
 
 or e.g. using a plugin manager like packer
 ```lua
     use { "CaitCatDev/ktree.git" }
 
-    --Later in plugin setup 
+    --Later in plugin setup
     require("ktree").setup({})
 ```
 
-## TODO: 
+## How to use:
+
+### Nvim User commands:
+
+- :KTreeOpen (Open the tree in a window)
+- :KTreeClose (Close the open tree window)
+- :KTreeRefresh (Refresh the contents of the tree)
+
+### Nvim keybinds:
+- a (Add a file at the current cursor postions)
+- r (Refresh the tree)
+- d (Remove the file/dir under the cursor)
+- <CR> (Open the current file/directory)
+
+
+## How to configure:
+An example configuration is here and this is the normal default config that we use.
+```lua
+	require("ktree").setup({
+		symbols = {
+			['directory'] = '',
+			['file'] = '',
+		}
+
+		window = {
+			width = 25,
+		},
+
+		colors = {
+			win_selfg = 0xf8f82,
+			win_selbg = "purple",
+			ktree_file = "cyan",
+			ktree_dir = "purple",
+			ktree_cwd = "green",
+		},
+
+		shiftwidth = 2,
+		tabstop = 2,
+		showwin = false,
+	})
+```
+
+## TODO:
 - [] Clean up code
-- [] Add ablility to create new dirs or files 
+- [x] Add ablility to create new dirs or files
 - [] Add rename
-- [] Currently user config is not supported 
+- [x] Currently user config is not supported
+- [] implement changing directory
--- init.lua
+++ init.lua
@@ -1,11 +1,27 @@
-local M = { } 
+local M = { }
 
 local config = {
-    symbols = {
-	['directory'] = '',
-	['file'] = '',
-    },
-    width = 25,
+	symbols = {
+		['directory'] = '',
+		['file'] = '',
+	},
+
+	window = {
+		width = 25,
+	},
+
+	--Colors
+	colors = {
+		win_selfg = 0xf8f8f2,
+		win_selbg = "purple",
+		ktree_file = "cyan",
+		ktree_dir = "purple",
+		ktree_cwd = "green"
+	},
+
+	shiftwidth = 2,
+	tabstop = 2,
+	showwin = false,
 }
 
 local state = { }
@@ -19,208 +35,356 @@ local state = { }
 --parent: String = "/home/cat"
 --
 
-local nodes = { } 
+local nodes = { }
 
 local state = { }
 
-local function merge_configs(user_config) 
-
+local function merge_configs(user_config)
+	config = vim.tbl_deep_extend("force", config, user_config)
 end
 
+
 local function create_node(nodeName, nodeType, nodeOpen, nodePath, depth)
-    
-    local node = { 
+	
+	local node = {
 	name = nodeName,
 	type = nodeType,
 	children = { },
 	open = nodeOpen,
 	parent = nodePath,
 	depth = depth
-    }
+	}
 
-    return node
+	return node
 end
 
 local function scandir(path, nodeTable, pos, depth) 
-    local fd = vim.loop.fs_scandir(path)
+	local fd = vim.loop.fs_scandir(path)
 	if fd then
 		while true do
-		    name, typ = vim.loop.fs_scandir_next(fd)
+			name, typ = vim.loop.fs_scandir_next(fd)
 			if name == nil then
 				break
 			end
-		    table.insert(nodeTable, pos, create_node(name, typ, false, path, depth))
-		    pos = pos + 1;
+			table.insert(nodeTable, pos, create_node(name, typ, false, path, depth))
+			pos = pos + 1;
 		end
 	end
 end
 
-local function ktree_refresh_window() 
-    local pos = vim.api.nvim_win_get_cursor(state.win)
-    if state.open == false then 
-	return 
-    end
-   
-    vim.api.nvim_buf_set_option(state.buf, 'modifiable', true)
+local function ktree_close_dir(row)
+	local to_remove = {}
 
-    vim.api.nvim_buf_set_lines(state.buf, 0, -1, true, {})
-    vim.api.nvim_buf_set_lines(state.buf, 0, 0, false, {vim.fn.getcwd()})
-    for k, v in pairs(nodes) do
-	local line = ""
-	for i = 1, v.depth do
-	    line = line .. '\t' 
+	if nodes[row - 1].open ~= true then
+		return
 	end
-	line = line .. config.symbols[v.type] .. " " .. v.name 
-	vim.api.nvim_buf_set_lines(state.buf, k, k, true, {line})
-	if v.type == 'file' then
-	    vim.api.nvim_buf_add_highlight(state.buf, -1, "KtreeFile", k, 0, -1)
-	elseif v.type == 'directory' then 
-	    vim.api.nvim_buf_add_highlight(state.buf, -1, "KtreeDir", k, 0, -1)
-	end
-    end
 
-    vim.api.nvim_buf_set_option(state.buf, 'modifiable', false)
-    vim.api.nvim_win_set_cursor(state.win, pos)
+	local parent = nodes[row].parent
+
+
+	for i,v in ipairs(nodes) do
+		if parent ~= v.parent then
+			table.insert(to_remove, v)
+		end
+	end
+
+	nodes[row - 1].open = false;
+	nodes = to_remove
 end
 
+local function ktree_open_dir(row)
+	if nodes[row-1].open then
+		ktree_close_dir(row)
+		return
+	end
 
-local function ktree_open_dir(row) 
-    local cwd = nodes[row - 1].parent;
-    print(nodes[row - 1].name)
+	local cwd = nodes[row - 1].parent;
+	print(nodes[row - 1].name)
 
-    scandir(cwd .. '/' .. nodes[row - 1].name, nodes, row, nodes[row - 1].depth + 1)
+	nodes[row-1].open = true;
+	scandir(cwd .. '/' .. nodes[row - 1].name, nodes, row, nodes[row - 1].depth + 1)
 end
 
-local function ktree_open_file(row) 
-    local winlist = vim.api.nvim_tabpage_list_wins(0)
-    local save_status = { } 
-    for i, win in ipairs(winlist) do
-	print(win, i)
-	table.insert(save_status, i, vim.wo[win].statusline)
-	vim.wo[win].statusline = string.format("%%=%c%%=", i + 64)
-    end
-    
-    vim.cmd("redraw!")
-    local char =  vim.fn.getchar()
+local function ktree_find_node_and_open(list, node)
+	for i, v in ipairs(list) do
+		if v.name == node.name then
+			v.open = true
+			scandir(v.parent .. "/" .. v.name, new_list, i+1, v.depth + 1)
+			return
+		end
+	end
+end
 
- 
-    for i, win in ipairs(winlist) do
-	vim.wo[win].statusline = save_status[i] 
-    end
+local function ktree_refresh_window()
+	local row,col = unpack(vim.api.nvim_win_get_cursor(state.win))
+	if state.open == false then
+		return
+	end
+	new_list = { }
 
-    if type(char) ~= "number" then
-	print("Unknown multi byte input")
-	return
-    end
+	scandir(vim.fn.getcwd(), new_list, 1, 1)
+	
+	for i, v in ipairs(nodes) do
+		if v.open then
+			ktree_find_node_and_open(new_list, v);
+		end
+	end
 
-    char = char - 96
-    print(char)
- 
-    if char <= #winlist and char > 0 then
+	vim.api.nvim_buf_set_option(state.buf, 'modifiable', true)
+
+	vim.api.nvim_buf_set_lines(state.buf, 0, -1, true, {})
+	vim.api.nvim_buf_set_lines(state.buf, 0, 0, false, {vim.fn.getcwd()})
+
+	vim.api.nvim_buf_add_highlight(state.buf, -1, "KTreeCWD", 0, 0, -1)
+
+	for k, v in pairs(new_list) do
+		local line = ""
+		
+		for i = 1, v.depth do
+			line = line .. '\t'
+		end
+		
+		line = line .. config.symbols[v.type] .. " " .. v.name
+		vim.api.nvim_buf_set_lines(state.buf, k, k, true, {line})
+		if v.type == 'file' then
+			vim.api.nvim_buf_add_highlight(state.buf, -1, "KTreeFile", k, 0, -1)
+		elseif v.type == 'directory' then
+			vim.api.nvim_buf_add_highlight(state.buf, -1, "KTreeDir", k, 0, -1)
+
+		end
+	end
+
+	nodes = new_list
+
+	vim.api.nvim_buf_set_option(state.buf, 'modifiable', false)
+	vim.api.nvim_win_set_cursor(state.win, {row, col})
+end
+
+local function ktree_add_file(input)
+	local row, col = unpack(vim.api.nvim_win_get_cursor(state.win))
+	local parent = nodes[row - 1].parent
+	if nodes[row-1].open then
+		parent = parent .. "/" .. nodes[row - 1].name
+	end
+	local char = string.sub(input, -1);
+
+	print(parent .. "/" .. input .. " " .. char)
+
+	if char == '/' then
+		vim.loop.fs_mkdir(parent .. "/" .. input, 510)
+	else
+		vim.loop.fs_open(parent .. "/" .. input, "w", 420);
+	end
+
+	ktree_refresh_window()
+end
+
+local function ktree_remove_file(input)
+	local row, col = unpack(vim.api.nvim_win_get_cursor(state.win))
+	local fname = nodes[row - 1].name
+	local parent = nodes[row - 1].parent
+
+	input = string.lower(input)
+	if input == 'n' then
+		return
+	end
+
+	if input ~= 'y' then
+		print(input .. " is not a valid option")
+		return
+	end
+	print(parent .. "/" .. fname)
+	if nodes[row - 1].type == 'directory' then
+		vim.loop.fs_rmdir(parent .. "/" .. fname)
+	else
+		vim.loop.fs_unlink(parent .. "/" .. fname)
+	end
+
+	ktree_refresh_window()
+end
+
+local function ktree_remove_file_input()
+	vim.ui.input({prompt = "Are you sure (y/n): "}, ktree_remove_file)
+end
+
+local function ktree_add_file_input() 
+	vim.ui.input({prompt = "File To Create: "}, ktree_add_file)
+end
+
+local function ktree_winlist_rm(list, value)
+	for i,win in ipairs(list) do
+		if win == value then
+			table.remove(list, i)
+		end
+	end
+end
+
+local function highlight(group, fg, bg)
+	vim.api.nvim_set_hl(0, group, {
+		fg = fg,
+		bg = bg,
+	})
+end
+
+local function ktree_open_file(row)
+	local winlist = vim.api.nvim_tabpage_list_wins(0)
+	local save_status = { }
+
+	if config.showwin == false then
+		ktree_winlist_rm(winlist, state.win)
+	end
+
+	--Save the old status line and replace it
+	for i, win in ipairs(winlist) do
+		print(win, i)
+		table.insert(save_status, i, vim.wo[win].statusline)
+		vim.wo[win].statusline = string.format("%%#KTreeWinSelect#%%=%c%%=", i + 64)
+	end
+
+	vim.cmd("redraw!")
+	local char = vim.fn.getchar()
+
+	--Restore the status lines of the windows.
+	for i, win in ipairs(winlist) do
+	vim.wo[win].statusline = save_status[i]
+	end
+
+	if type(char) ~= "number" then
+		print("Unknown multi byte input")
+		return
+	end
+
+	--Convert ASCII Code to number
+	char = char - 96
+
+	if char <= #winlist and char > 0 then
 	vim.api.nvim_set_current_win(winlist[char])
 	vim.cmd("e " .. nodes[row-1].parent .. "/" .. nodes[row-1].name)
-    else 
+	else
 	print("Unknown input " .. char + 96);
-    end
+	end
 
 end
 
-local function ktree_open_node()  
-    local row, col = unpack(vim.api.nvim_win_get_cursor(state.win))
-    print(nodes[row - 1].name)
-   
-    if nodes[row - 1].type == 'directory' then 
-	ktree_open_dir(row)
-    elseif nodes[row - 1].type == 'file' then
-	ktree_open_file(row)
-    end
+local function ktree_open_node()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(state.win))
+	if row == 1 then
+		ktree_refresh_window()
+		return
+	end
+
+	print(nodes[row - 1].name)
+
+	if nodes[row - 1].type == 'directory' then
+		ktree_open_dir(row)
+	elseif nodes[row - 1].type == 'file' then
+		ktree_open_file(row)
+	end
 
 
-    ktree_refresh_window()
+	ktree_refresh_window()
 end
 
 local function ktree_open_window()
-    if state.open == true then
-	return 
-    end
-    vim.cmd("vsplit")
-    vim.api.nvim_win_set_buf(0, state.buf)
-    
-    state.win = vim.api.nvim_get_current_win()
-    vim.cmd("set nonumber") 
-    vim.api.nvim_win_set_width(state.win, config.width)
+	if state.open == true then
+	return
+	end
+	vim.cmd("vsplit");
 
-    state.open = true
-    ktree_refresh_window()
+	vim.api.nvim_win_set_buf(0, state.buf)
+
+	state.win = vim.api.nvim_get_current_win()
+	vim.wo[state.win].list = false
+	vim.wo[state.win].wrap = false
+
+	vim.cmd("set nonumber")
+	vim.api.nvim_win_set_width(state.win, config.window.width)
+
+	state.open = true
+	ktree_refresh_window()
 end
 
 
-local function ktree_close_window() 
-    if state.open == false then 
-	return 
-    end
-    
-    vim.api.nvim_win_close(state.win, false);
-    state.win = nil
-    state.open = false 
+local function ktree_close_window()
+	if state.open == false then
+	return
+	end
+
+	vim.api.nvim_win_close(state.win, false);
+	state.win = nil
+	state.open = false
 end
 
-local function ktree_setup(user_config) 
-    state.buf = vim.api.nvim_create_buf(false, true) 
-    scandir(vim.fn.getcwd(), nodes, 1, 1)
-    
-    vim.api.nvim_create_user_command("KtreeOpen", ktree_open_window, {})
-    vim.api.nvim_create_user_command("KtreeClose", ktree_close_window, {})
-    vim.api.nvim_create_user_command("KtreeRefresh", ktree_refresh_window, {}); 
+local function ktree_setup(user_config)
+	merge_configs(user_config)
 
-    vim.api.nvim_buf_set_keymap(state.buf, "", "<CR>", "", {
-	noremap = false,
-	callback = ktree_open_node,
-	desc = "Open the current line node",
-    })
+	state.buf = vim.api.nvim_create_buf(false, true)
+	scandir(vim.fn.getcwd(), nodes, 1, 1)
 
-    vim.api.nvim_buf_set_keymap(state.buf, "", "r", "", {
-	noremap = false,
-	callback = ktree_refresh_window,
-	desc = "Open the current line node",
-    })
+	vim.api.nvim_create_user_command("KTreeOpen", ktree_open_window, {})
+	vim.api.nvim_create_user_command("KTreeClose", ktree_close_window, {})
+	vim.api.nvim_create_user_command("KTreeRefresh", ktree_refresh_window, {});
 
-    --Setup highlighting 
-    --TODO: set highlight namespace
-    vim.api.nvim_set_hl(0, "KtreeFile", {
-	fg = "cyan",
-	bg = "NONE",
-	bold = true,
-    })
-    vim.api.nvim_set_hl(0, "KtreeDir", {
-	fg = "purple",
-	bg = "NONE",
-	bold = true,
-    })
-    
-    vim.api.nvim_buf_set_option(state.buf, 'modifiable', false)
-    
-    --Listen for when a window with this buffer is closed
-    vim.api.nvim_create_autocmd({"WinClosed"}, {
+	vim.api.nvim_buf_set_keymap(state.buf, "", "a", "", { 
+		noremap = false,
+		callback =  ktree_add_file_input,
+		desc = "add file at cursor pos"
+	})
+
+	vim.api.nvim_buf_set_keymap(state.buf, "", "<CR>", "", {
+		noremap = false,
+		callback = ktree_open_node,
+		desc = "Open the current line node",
+	})
+
+	vim.api.nvim_buf_set_keymap(state.buf, "", "r", "", {
+		noremap = false,
+		callback = ktree_refresh_window,
+		desc = "Open the current line node",
+	})
+
+	vim.api.nvim_buf_set_keymap(state.buf, "", "d", "", {
+		noremap = false,
+		callback =ktree_remove_file_input,
+		desc = "Delete the file under the cursor"
+	})
+
+	--Setup highlighting 
+	--TODO: set highlight namespace
+	highlight("KTreeFile", config.colors.ktree_file, "NONE")
+	highlight("KTreeDir", config.colors.ktree_dir, "NONE")
+	highlight("KTreeWinSelect", config.colors.win_selfg, config.colors.win_selbg)
+	highlight("KTreeCWD", config.colors.ktree_cwd, "NONE");
+	vim.api.nvim_buf_set_option(state.buf, 'modifiable', false)
+
+	--Listen for when a window with this buffer is closed
+	vim.api.nvim_create_autocmd({"WinClosed"}, {
 	buffer = state.buf,
-	callback = function(ev) 
-	    state.open = false
-	    state.win = nil
+	callback = function(ev)
+		state.open = false
+		state.win = nil
 	end
-    })
-    vim.api.nvim_create_autocmd({"BufWinLeave"}, {
-	buffer = state.buf,
-	callback = function(ev) 
-	    state.open = false
-	    state.win = nil
-	end
-    })
+	})
 
-    vim.api.nvim_buf_set_name(state.buf, 'Ktree')
+	vim.api.nvim_create_autocmd({"BufWinLeave"}, {
+	buffer = state.buf,
+	callback = function(ev)
+		state.open = false
+		state.win = nil
+	end
+	})
+
+-----
+--	Buffer Options
+-----
+	vim.bo[state.buf].tabstop = config.tabstop
+	vim.bo[state.buf].shiftwidth = config.shiftwidth
+
+	vim.api.nvim_buf_set_name(state.buf, 'KTree')
 end
 
 M = {
-    setup = ktree_setup,
+	setup = ktree_setup,
 }
 
 return M
